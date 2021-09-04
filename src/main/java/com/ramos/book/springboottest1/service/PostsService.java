fd523733648870fd0647cf7e04e6c268866ec622
alanhakhyeonsong <songs4805@naver.com>
2021-09-05 00:18:07 +0900

    springboot-test1

---
+++ src/main/java/com/ramos/book/springboottest1/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.ramos.book.springboottest1.service;
+
+import com.ramos.book.springboottest1.domain.posts.PostsRepository;
+import com.ramos.book.springboottest1.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
