b41370459fd97ffb678186288801767b4d80cc2f
alanhakhyeonsong <songs4805@naver.com>
2021-09-04 22:03:40 +0900

    springboot_test1

---
+++ src/test/java/com/ramos/book/springboottest1/SpringbootTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.book.springboottest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
