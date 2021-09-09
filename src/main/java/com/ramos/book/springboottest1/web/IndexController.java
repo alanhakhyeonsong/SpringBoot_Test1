112f93debffda7674b56ee2d04909d9043c0e4c2
alanhakhyeonsong <songs4805@naver.com>
2021-09-09 15:41:09 +0900

    refactoring

--- src/main/java/com/ramos/book/springboottest1/web/IndexController.java
+++ src/main/java/com/ramos/book/springboottest1/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.ramos.book.springboottest1.web;
 
+import com.ramos.book.springboottest1.config.auth.LoginUser;
 import com.ramos.book.springboottest1.config.auth.dto.SessionUser;
 import com.ramos.book.springboottest1.service.PostsService;
 import com.ramos.book.springboottest1.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
